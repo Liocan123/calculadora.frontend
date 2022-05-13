38730ab2a3e4a1a61ba45bec2b1770fc6b285c52
Liocan123 <nicolay.lopez@alumnos.uv.cl>
2022-05-13 12:32:16 -0400

    Up

---
+++ README.md
@@ -0,0 +1,9 @@
+# calculadora.frontend
+
+
+Integrantes:
+
+Amanda Valentina Díaz Rojas: Amyyinthesky |
+Constanza Carolina Vivar muñoz: Constanzavm |
+Mauricio Alejandro Seguel Carreño: IronWesso |
+Nicolay Mario López Imbarack: Liocan123
---
+++ js/calculadora.js
@@ -0,0 +1,39 @@
+function siete(){
+    document.Calculadora.ans.value+='7';
+};
+
+function seis(){
+    document.Calculadora.ans.value+='6';
+};
+
+function cinco(){
+    document.Calculadora.ans.value+='5';
+};
+
+function cuatro(){
+    document.Calculadora.ans.value+='4';
+};
+
+function tres(){
+    document.Calculadora.ans.value+='3';
+};
+
+function dos(){
+    document.Calculadora.ans.value+='2';
+};
+
+function uno(){
+    document.Calculadora.ans.value+='1';
+};
+
+function cero(){
+    document.Calculadora.ans.value+='0';
+};
+
+function ocho(){
+    document.Calculadora.ans.value+='8';
+};
+
+function nueve(){
+    document.Calculadora.ans.value+='9';
+};
